4355ee13ccc14a687bfee1e417a7cf077bf2535e
Imacop42 <60049895+Imacop42@users.noreply.github.com>
2020-02-05 23:01:51 -0500

    everything

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-#!/usr/bin/env python3
 import sys, os, json
-# Check to make sure we are running the correct version of Python
 assert sys.version_info >= (3,7), "This script requires at least Python 3.7"
 
-# The game and item description files (in the same folder as this script)
-game_file = 'zork.json'
+
+game_file = 'document.json'
 item_file = 'items.json'
+inventory = []
 
 
-# Load the contents of the files into the game and items dictionaries. You can largely ignore this
-# Sorry it's messy, I'm trying to account for any potential craziness with the file location
+
 def load_files():
     try:
         __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -20,15 +18,123 @@ def load_files():
         print("There was a problem reading either the game or item file.")
         os._exit(1)
 
-# The main function for the game
+def check_inventory(item):
+    for i in inventory:
+        if i == item:
+            return True
+        return False
+
+
+
+def render(game,items,current):
+    c = game[current]
+    print("\n\n" + c["name"])
+    print("\n\n "+ c["desc"])
+  
+    # Display any items
+    for item in c["items"]:
+        if not check_inventory(item["item"]):
+            print(item["desc"])
+    
+    #display item information
+    for i in inventory:
+        if i in items:
+            if current in items[i]["exits"]:
+                print(items[i]["exits"][current])
+
+
+
+
+
+
+def get_input():
+    response = input("\nWhat would you like to do? ")
+    response = response.upper().strip()
+    return response
+
+
+
+def update(game,items,current,response):
+    #inventory check command
+    if response == "INVENTORY":
+        print("You are carrying: ")
+        if len(inventory) == 0:
+            print("Nothing")
+        else:
+            for i in inventory:
+                print(i.lower())
+        return current
+
+    #movement command
+    c = game[current]
+    for e in c["exits"]:
+        if response == e["exit"]:
+            return e["target"]
+
+    #pick up command
+    for item in c["items"]:
+        if response == "GET " + item["item"] and not check_inventory(item["item"]):
+            print(item["take"])
+            inventory.append(item["item"])
+            return current
+        if response == "PICK UP " + item["item"] and not check_inventory(item["item"]):
+            print(item["take"])
+            inventory.append(item["item"])
+            return current
+        if response == "TAKE " + item["item"] and not check_inventory(item["item"]):
+            print(item["take"])
+            inventory.append(item["item"])
+            return current
+    
+    #actions for items
+    for i in inventory:
+        if i in items:
+            for action in items[i]["actions"]:
+                if response == action + " " + i:
+                    print(items[i]["actions"][action])
+                    return current       
+
+    return current
+
+
+
+
+# MAIN GAME FUNCTION
 def main():
-    current = 'WHOUS'  # The starting location
-    end_game = ['END']  # Any of the end-game locations
+    current = 'FIELD'  #START
+    end_game = ['END']  #ENDGAME AREAS
 
     (game,items) = load_files()
 
-    # Add your code here
 
-# run the main function
+
+
+    while True:
+        render(game,items,current)
+        response = get_input()
+
+        if response == "QUIT":
+            break
+
+        current = update(game,items,current,response)
+    
+    print("Thanks for playing!")
+    
+
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
 	main()
